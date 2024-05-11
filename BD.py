4e7c80ab1caca74b5ccc7dfc347c34ff296d0046
Juan Manuel <kedipert27@gmail.com>
2024-05-11 00:01:47 -0500

    CRUD LISTO SOLO EN BD Y DATOS QUEMADOS

--- BD.py
+++ BD.py
@@ -1,11 +1,181 @@
 import sqlite3
 
 def conectar():
-    conexion = sqlite3.connect("agendaBD")
+    conexion = sqlite3.connect("agendaBD.sqlite3")
     cursor = conexion.cursor()
-    return conexion,cursor
-def crearTabla():
-    conexion,cursor = conectar()
+    return conexion, cursor
+#-------------------------------------------------------------------------------------------------------------------------------------
+def crearTablaContactos():
+    conexion, cursor = conectar()
     sql = """    
-        
+        CREATE TABLE IF NOT EXISTS contacto(
+            id_contacto INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            id_usuario int NOT NULL,
+            nombre_contacto VARCHAR(60) NOT NULL,
+            apellido_contacto VARCHAR(60) NOT NULL,
+            email VARCHAR(20) NOT NULL,
+            CONSTRAINT id_de_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+        )
     """
+    if cursor.execute(sql):
+        print("Tabla De Contacto Creada")
+    else:
+        print("No se pudo crear la tabla ")
+    conexion.close()
+
+def crearTablaUsuarios():
+    conexion, cursor = conectar()
+    sql = """    
+        CREATE TABLE IF NOT EXISTS usuario(
+            id_usuario INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            nombre_usuario NOT NULL,
+            apellido_usuario VARCHAR(60) NOT NULL,
+            apellido_usuario VARCHAR(60) NOT NULL,
+            correo_electronico VARCHAR(20) UNIQUE NOT NULL,
+            contrasena VARCHAR(20) NOT NULL,
+            direccion VARCHAR(20) NOT NULL
+        )
+    """
+    if cursor.execute(sql):
+        print("Tabla De Usuario  Creada")
+    else:
+        print("No se pudo crear la tabla ")
+    conexion.close()
+
+def crearTablaCategoriaPorContacto():
+    conexion, cursor = conectar()
+    sql = """    
+        CREATE TABLE IF NOT EXISTS categorias_por_contacto(
+            id_categoria int NOT NULL,
+            id_contacto int NOT NULL,
+            CONSTRAINT id_de_categoria FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria),
+            CONSTRAINT id_de_contacto FOREIGN KEY (id_contacto) REFERENCES contacto(id_contacto)
+        
+        )
+    """
+    if cursor.execute(sql):
+        print("Tabla De Usuario  Creada")
+    else:
+        print("No se pudo crear la tabla ")
+    conexion.close()
+
+
+def crearTablaCategoria():
+    conexion, cursor = conectar()
+    sql = """    
+        CREATE TABLE IF NOT EXISTS categoria(
+            id_categoria INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            nombre_categoria VARCHAR(20) NOT NULL
+        )
+    """
+    if cursor.execute(sql):
+        print("Tabla De Usuario  Creada")
+    else:
+        print("No se pudo crear la tabla ")
+    conexion.close()
+def crearTablaCategoria():
+    conexion, cursor = conectar()
+    sql = """    
+        CREATE TABLE IF NOT EXISTS categoria(
+            id_categoria INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            nombre_categoria VARCHAR(20) NOT NULL
+        )
+    """
+    if cursor.execute(sql):
+        print("Tabla De Usuario  Creada")
+    else:
+        print("No se pudo crear la tabla ")
+    conexion.close()
+
+def crearTablaCategoriaPorContacto():
+    conexion, cursor = conectar()
+    sql = """    
+        CREATE TABLE IF NOT EXISTS categorias_por_contacto(
+            id_categoria int NOT NULL,
+            id_contacto int NOT NULL,
+            CONSTRAINT id_de_categoria FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria),
+            CONSTRAINT id_de_contacto FOREIGN KEY (id_contacto) REFERENCES contacto(id_contacto)
+        )
+    """
+    if cursor.execute(sql):
+        print("Tabla De Categorias_por_contacto Creada")
+    else:
+        print("No se pudo crear la tabla de categorias_por_contacto")
+    conexion.close()
+
+##--------------------------------------------------------------------------------------------------------------------
+# CRUD DEL CONTACTO
+def insertarContacto(datos):
+    conexion, cursor = conectar()
+    sql = """
+    INSERT INTO Contactos(nombre,apellido,telefono,email) VALUES(?,?,?,?)
+    """
+    if cursor.execute(sql, datos):
+        print("Datos guardados")
+    else:
+        print("No se pudieron guardar los datos ")
+    conexion.commit()
+    conexion.close()
+
+def consultarContacto():
+    conexion, cursor = conectar()
+    sql = "SELECT id,nombre,apellido,telefono,email FROM Contactos"
+    cursor.execute(sql)
+    listado = []
+    for fila in cursor:
+        listado.append(fila)    
+    conexion.close()
+    return listado
+
+def modificarContacto(id, nombre, apellido, telefono, email):
+    conexion, cursor = conectar()
+    sql = """UPDATE agenda 
+             SET nombre=?, apellido=?, telefono=?, email=? 
+             WHERE id=?"""
+    cursor.execute(sql, (nombre, apellido, telefono, email, id))
+    conexion.commit()
+    conexion.close()
+
+def borrarContacto(id):
+    conexion, cursor = conectar()
+    sql = "DELETE FROM agenda WHERE id = ?"
+    cursor.execute(sql, (id,))
+    conexion.commit()
+    conexion.close()
+##--------------------------------------------------------------------------------------------------------------------
+## LOGIN CREAR USUARIO
+def insertarUsuario(datos):
+    conexion, cursor = conectar()
+    sql = """
+    INSERT INTO usuario(nombre_usuario, apellido_usuario, correo_electronico, contrasena, direccion) VALUES(?, ?, ?, ?, ?)
+    """
+    if cursor.execute(sql, datos):
+        print("Datos guardados")
+    else:
+        print("No se pudieron guardar los datos  del usuario")
+    conexion.commit()
+    conexion.close()
+def borrarUsuario(id):
+    conexion, cursor = conectar()
+    sql = "DELETE FROM usuario WHERE id_usuario = ?"
+    cursor.execute(sql, (id))
+    conexion.commit()
+    conexion.close()
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+#INSERTAR DATOS QUEMADOS
+def insertarCategoria(datos):
+    conexion, cursor = conectar()
+    sql = """
+    INSERT INTO categoria(nombre_categoria) VALUES(?)
+    """
+    if cursor.execute(sql, datos):
+        print("Datos guardados")
+    else:
+        print("No se pudieron guardar los datos  del usuario")
+    conexion.commit()
+    conexion.close()
+##QUEMAR ARCHIVOS 
+nombre_categoria = "OTROS"
+insertarCategoria((nombre_categoria,))
--- agenda_Contactos.py
+++ agenda_Contactos.py
@@ -1,50 +1,110 @@
 from tkinter import *
+from tkinter import messagebox
+from BD import *
+
 ANCHO = 560
 ALTO = 540
 POSX = 400
 POSY = 400
-anchoAlto = str(ANCHO)+"x"+str(ALTO)
-posicionX = "+"+str(POSX)
-posicionY = "+"+str(POSY)
+
+def mostrarMensaje(titulo, mensaje):
+    messagebox.showinfo(titulo, mensaje)
+
+def limpiarDatos():
+    nombre.set("")
+    apellido.set("")
+    telefono.set("")
+    email.set("")
+    text.delete(1.0, END)
+
+def guardarDatos():
+    crearTablaContactos()
+    if nombre.get() == "" or apellido.get() == "":
+        mostrarMensaje("ERROR", "Debes rellenar Los datos")
+    else:
+        datos = nombre.get(), apellido.get(), telefono.get(), email.get()
+        mostrarMensaje("Guardar", "Contacto Guardado")
+        insertarContacto(datos)
+        limpiarDatos()
+
+def actualizar():
+    crearTablaContactos()
+    if ID.get() == "" and nombre.get() == "":
+        mostrarMensaje("Error", "Debes rellenar los datos ")
+    else:
+        modificarContacto(ID.get(), nombre.get(), apellido.get(), telefono.get(), email.get())
+        mostrarMensaje("Modificacion", "Se han modificado los datos")
+        limpiarDatos()
+
+def borrar_registro():
+    if ID.get() == "":
+        mostrarMensaje("Error", "No se encontro el contacto")
+    else:
+        borrarContacto(ID.get())
+        mostrarMensaje("Borrar", "Se ha borrado el contacto")
+        limpiarDatos()
+
+def mostrar():
+    listado = consultarContacto()
+    text.delete(1.0, END)
+    text.insert(INSERT, "ID\tNombre\tApellido\t\tTelefono\tEmail\n")
+    for elemento in listado:
+        id_ = elemento[0]
+        nombre_ = elemento[1]
+        apellido_ = elemento[2]
+        telefono_ = elemento[3]
+        email_ = elemento[4]
+        text.insert(INSERT, id_)
+        text.insert(INSERT, "\t")
+        text.insert(INSERT, nombre_)
+        text.insert(INSERT, "\t")
+        text.insert(INSERT, apellido_)
+        text.insert(INSERT, "\t\t")
+        text.insert(INSERT, telefono_)
+        text.insert(INSERT, "\t")
+        text.insert(INSERT, email_)
+        text.insert(INSERT, "\n")
+
+anchoAlto = str(ANCHO) + "x" + str(ALTO)
+posicionX = "+" + str(POSX)
+posicionY = "+" + str(POSY)
 colorVentana = "blue"
-colorFondo="blue"
-colorLetra="white"
-ventana= Tk()
+
+ventana = Tk()
 ventana.config(bg=colorVentana)
-ventana.geometry(anchoAlto+posicionX+posicionY)
+ventana.geometry(anchoAlto + posicionX + posicionY)
 ventana.title("Agenda")
+
 frame = Frame()
-frame.config(width=ANCHO,height=ALTO)
-frame.config(bg="darkblue")
+frame.config(width=ANCHO, height=ALTO, bg="darkblue")
 frame.pack()
-#Vriables
+
 ID = IntVar()
 nombre = StringVar()
 apellido = StringVar()
 telefono = StringVar()
 email = StringVar()
-#widgets
-etiquetaID= Label(frame,text="ID: ").place(x=50,y=50)
-cajaID = Entry(frame,textvariable=ID).place(x=130,y=50)
-etiquetaNombre = Label(frame,text="Nombre: ").place(x=50,y=90)
-cajaNombre = Entry(frame,textvariable="nombre").place(x=130,y=90)
-etiquetaApellido = Label(frame,text="Apellido:").place(x=50,y=130)
-cajaApellido = Entry(frame,textvariable="apellido").place(x=130,y=130)
-etiquetaTelefono = Label(frame,text="Telefono").place(x=50,y=170)
-cajaTelefono = Entry(frame,textvariable="telefono").place(x=130,y=170)
-etiquetaEmail = Label(frame,text="Email: ").place(x=50,y=210)
-cajaEmail = Entry(frame,textvariable="email").place(x=130,y=210)
+
+etiquetaID = Label(frame, text="ID: ").place(x=50, y=50)
+cajaID = Entry(frame, textvariable=ID).place(x=130, y=50)
+etiquetaNombre = Label(frame, text="Nombre: ").place(x=50, y=90)
+cajaNombre = Entry(frame, textvariable=nombre).place(x=130, y=90)
+etiquetaApellido = Label(frame, text="Apellido:").place(x=50, y=130)
+cajaApellido = Entry(frame, textvariable=apellido).place(x=130, y=130)
+etiquetaTelefono = Label(frame, text="Telefono").place(x=50, y=170)
+cajaTelefono = Entry(frame, textvariable=telefono).place(x=130, y=170)
+etiquetaEmail = Label(frame, text="Email: ").place(x=50, y=210)
+cajaEmail = Entry(frame, textvariable=email).place(x=130, y=210)
+
 text = Text(frame)
-text.place(x=50,y=240,width=500,height=200)
-botonAñadir= Button(frame,text="Añadir").place(x=150,y=500)
-botonBorrar= Button(frame,text="Borrar").place(x=200,y=500)
-botonConsultar= Button(frame,text="Consultar").place(x=250,y=500)
-botonModificar= Button(frame,text="Actualizar").place(x=320,y=500)
+text.place(x=50, y=240, width=500, height=200)
 
-
+botonAñadir = Button(frame, text="Añadir", command=guardarDatos).place(x=150, y=500)
+botonBorrar = Button(frame, text="Borrar", command=borrar_registro).place(x=200, y=500)
+botonConsultar = Button(frame, text="Consultar", command=mostrar).place(x=250, y=500)
+botonModificar = Button(frame, text="Actualizar", command=actualizar).place(x=320, y=500)
 
 ventana.mainloop()
-
 
 
 

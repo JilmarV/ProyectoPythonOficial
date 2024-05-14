2acc22b5ccdb5def0e83fcf3451648ba011b76c6
jilmar <veloza.jilmar.2393@eam.edu.co>
2024-05-13 22:04:53 -0500

    tablas nuevas BD

--- BD_tablas_sql.py
+++ BD_tablas_sql.py
@@ -6,11 +6,13 @@ def crearTablaContactos():
     sql = """    
         CREATE TABLE IF NOT EXISTS contacto(
             id_contacto INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            id_categoria int,
             id_usuario int NOT NULL,
             nombre_contacto VARCHAR(60) NOT NULL,
             apellido_contacto VARCHAR(60) NOT NULL,
             email VARCHAR(20) NOT NULL,
-            CONSTRAINT id_de_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+            CONSTRAINT id_de_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+            CONSTRAINT id_de_categoria FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria)
         )
     """
     if cursor.execute(sql):
@@ -80,22 +82,6 @@ def crearTablaCategoria():
         print("Tabla De Usuario  Creada")
     else:
         print("No se pudo crear la tabla ")
-    conexion.close()
-
-def crearTablaCategoriaPorContacto():
-    conexion, cursor = Conexion.conectar()
-    sql = """    
-        CREATE TABLE IF NOT EXISTS categorias_por_contacto(
-            id_categoria int NOT NULL,
-            id_contacto int NOT NULL,
-            CONSTRAINT id_de_categoria FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria),
-            CONSTRAINT id_de_contacto FOREIGN KEY (id_contacto) REFERENCES contacto(id_contacto)
-        )
-    """
-    if cursor.execute(sql):
-        print("Tabla De Categorias_por_contacto Creada")
-    else:
-        print("No se pudo crear la tabla de categorias_por_contacto")
     conexion.close()
 
 crearTablaContactos()

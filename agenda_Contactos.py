f623e348249075722afc8fcecf282aa2afec8217
jilmar <veloza.jilmar.2393@eam.edu.co>
2024-05-16 00:13:37 -0500

    vea su gestionde contactos modificada lindo

--- agenda_Contactos.py
+++ agenda_Contactos.py
@@ -6,79 +6,140 @@ class agenda(Toplevel):
     def __init__(self,id, ventana_padre, *args, **kwargs):
         super().__init__(ventana_padre)
         self.id_usuario = id
-        self.ANCHO = 560
-        self.ALTO = 600
-        self.POSX = 400
-        self.POSY = 400
-        self.anchoAlto = str(self.ANCHO) + "x" + str(self.ALTO)
-        self.posicionX = "+" + str(self.POSX)
-        self.posicionY = "+" + str(self.POSY)
+        self.title("Agenda")
+        
+        # Obtener dimensiones de la pantalla
+        ancho_pantalla = self.winfo_screenwidth()
+        alto_pantalla = self.winfo_screenheight()
+        
+        # Dimensiones de la ventana
+        ancho_ventana = 800
+        alto_ventana = 700
+        
+        x = (ancho_pantalla // 2) - (ancho_ventana // 2)
+        y = (alto_pantalla // 2) - (alto_ventana // 2)
+        self.geometry(f"{ancho_ventana}x{alto_ventana}+{x}+{y}")
         
         self.colorVentana = "#01172F"
         self.color_boton = "#08A4BD"
         self.color_boton_secundario = "#00635D"
-
-        self.config(bg=self.colorVentana)
-        self.geometry(self.anchoAlto + self.posicionX + self.posicionY)
-        self.title("Agenda")
-
-        self.frame = Frame(self)
-        self.frame.config(width=self.ANCHO, height=self.ALTO, bg=self.colorVentana)
-        self.frame.pack()
+        
+        #--------------------------------
+        #       parte frames
+        #--------------------------------
+        
+        self.frame = Frame(self, bg=self.colorVentana)
+        self.frame.pack(fill="both", expand=True)
+        self.frame.columnconfigure(0, weight=1)
+        self.frame.columnconfigure(1, weight=1)
+        
+        #--------------------------------
+        #       parte del titulo
+        #--------------------------------
         
         self.titulo = Label(self.frame,
-                            text="GESTION DE CONTACTOS",
+                            text="Gestion de contactos",
                             font=("Calisto MT", 26, "bold"),
                             bg=self.colorVentana,
                             fg="white")
-        self.titulo.place(x=40, y=10)
-
+        self.titulo.grid(row=1, column=0, columnspan=2, pady=20)
+        
+        #--------------------------------
+        #       parte datos
+        #--------------------------------
+        
         self.nombre = StringVar()
         self.apellido = StringVar()
         self.email = StringVar()
         self.telefonoUno = IntVar()
         self.telefonoDos = StringVar()
         
-        self.etiquetaNombre = Label(self.frame, text="Nombre: ", bg=self.colorVentana, fg="white").place(x=50, y=90)
-        self.cajaNombre = Entry(self.frame, textvariable=self.nombre).place(x=130, y=90)
-        self.etiquetaApellido = Label(self.frame, text="Apellido:", bg=self.colorVentana, fg="white").place(x=50, y=130)
-        self.cajaApellido = Entry(self.frame, textvariable=self.apellido).place(x=130, y=130)
-        self.etiquetaTelefono = Label(self.frame, text="Telefono:", bg=self.colorVentana, fg="white").place(x=50, y=170)
-        self.cajaTelefono = Entry(self.frame, textvariable=self.telefonoUno).place(x=130, y=170)
-        self.etiquetaEmail = Label(self.frame, text="Email: ", bg=self.colorVentana, fg="white").place(x=50, y=210)
-        self.cajaEmail = Entry(self.frame, textvariable=self.email).place(x=130, y=210)
+        self.etiquetaNombre = Label(self.frame,
+                                    text="Nombre: ",
+                                    bg=self.colorVentana,
+                                    fg="white")
+        self.etiquetaNombre.grid(row = 2, column = 0, columnspan = 1, padx = 10, sticky = "e")
+        
+        self.cajaNombre = Entry(self.frame,
+                                textvariable=self.nombre)
+        self.cajaNombre.grid(row = 2, column = 1, columnspan = 3, padx = 10, sticky = "w")
+        
+        self.etiquetaApellido = Label(self.frame,
+                                      text="Apellido:",
+                                      bg=self.colorVentana,
+                                      fg="white")
+        self.etiquetaApellido.grid(row = 3, column = 0, padx = 10, sticky = "e")
+        
+        self.cajaApellido = Entry(self.frame,
+                                  textvariable=self.apellido)
+        self.cajaApellido.grid(row = 3, column = 1, columnspan = 3, padx = 10, sticky = "w")
+        
+        self.etiquetaTelefono = Label(self.frame,
+                                      text="Telefono:",
+                                      bg=self.colorVentana,
+                                      fg="white")
+        self.etiquetaTelefono.grid(row = 4, column = 0, padx = 10, sticky = "e")
+        
+        self.cajaTelefono = Entry(self.frame,
+                                  textvariable=self.telefonoUno)
+        self.cajaTelefono.grid(row = 4, column = 1, columnspan = 3, padx = 10, sticky = "w")
         
         self.label_telefonoDos = Label(self.frame,
                                                 text = "Telefono 2:",
                                                 bg = self.colorVentana,
                                                 fg = "white")
-        self.label_telefonoDos.place(x = 300, y =170 )
+        self.label_telefonoDos.grid(row = 5, column = 0, padx = 10, sticky = "e")
         
         self.entry_telefonoDos = Entry(self.frame,
-                                                    bd = 0,
-                                                    width = 20,
-                                                    textvariable=self.telefonoDos)
-        self.entry_telefonoDos.place(x = 400, y =170 )
+                                       textvariable=self.telefonoDos)
+        self.entry_telefonoDos.grid(row = 5, column = 1, columnspan = 3, padx = 10, sticky = "w")
         
-        self.etiquetaCombo = Label(self.frame, text="Categoría: ", bg=self.colorVentana, fg="white").place(x=300, y=90)
+        self.etiquetaEmail = Label(self.frame,
+                                   text="Email: ",
+                                   bg=self.colorVentana,
+                                   fg="white")
+        self.etiquetaEmail.grid(row = 6, column = 0, padx = 10, sticky = "e")
+        
+        self.cajaEmail = Entry(self.frame,
+                               textvariable=self.email)
+        self.cajaEmail.grid(row = 6, column = 1, columnspan = 3, padx = 10, sticky = "w")
+        
+        self.etiquetaCombo = Label(self.frame,
+                                   text="Categoría: ",
+                                   bg=self.colorVentana,
+                                   fg="white")
+        self.etiquetaCombo.grid(row = 7, column = 0, padx = 10, sticky = "e")
+        
         self.combo = ttk.Combobox(self.frame)
-        self.combo.place(x=400,y=90)
+        self.combo.grid(row = 7, column = 1, columnspan = 3, padx = 10, sticky = "w")
+        
         listaCategoria =  consultarCategoria()
         self.combo['values'] = listaCategoria
         self.combo.current(0)
         #   metodo para cuando el suaurio cambie de seleccion cargarCategoria()
         self.text = Text(self.frame)
-        self.text.place(x=50, y=240, width=650, height=300)
+        self.text.place(x=80, y=240, width=650, height=300)
         self.categoria = self.combo.current()
         self.combo.bind("<<ComboboxSelected>>",self.cargarCategoria)
-        botonAñadir = Button(self.frame, text="Añadir", command=self.guardarDatos, background=self.color_boton).place(x=150, y=500)
-        botonBorrar = Button(self.frame, text="Borrar", command=self.borrar_registro, background=self.color_boton).place(x=200, y=500)
-        botonConsultar = Button(self.frame, text="Consultar", command=self.mostrar, background=self.color_boton).place(x=250, y=500)
-        botonModificar = Button(self.frame, text="Actualizar", command=self.actualizar, background=self.color_boton).place(x=320, y=500)
+        
+        botonAñadir = Button(self.frame, text="Añadir", command=self.guardarDatos, background=self.color_boton)
+        botonAñadir.place(x=200, y=600)
+        
+        botonBorrar = Button(self.frame, text="Borrar", command=self.borrar_registro, background=self.color_boton)
+        botonBorrar.place(x=250, y=600)
+        
+        botonConsultar = Button(self.frame, text="Consultar", command=self.mostrar, background=self.color_boton)
+        botonConsultar.place(x=300, y=600)
+        
+        botonModificar = Button(self.frame, text="Actualizar", command=self.actualizar, background=self.color_boton)
+        botonModificar.place(x=370, y=600)
+        
         boton_atras = Button(self.frame,
                                           text = "Atras",
                                           background = self.color_boton_secundario,
-                                          command=self.atras).place(x=420, y=500)
+                                          command=self.atras)
+        boton_atras.place(x=500, y=600)
+        
     def on_selected(self):
         self.cargarCategoria(self.id_usuario,self.combo.current())
     
@@ -86,7 +147,7 @@ class agenda(Toplevel):
     def cargarCategoria(self,event):
         listado = obtenerContactosPorUsuarioYCategoria(self.id_usuario,self.combo.current())
         self.text.delete(1.0, END)
-        self.text.insert(INSERT, "ID_Categoria\tID_Usuario\tNombre_Contacto\tApellido_Contacto\t\tEmail\tTelefono_Uno\tTelefono_Dos\n")
+        self.text.insert(INSERT, "Id Categoria\tID Usuario\tNombre\tApellido\t\tEmail\tTelefono Uno\tTelefono Dos\n")
         for elemento in listado:
             id_categoria_ = elemento[0]
             id_usuario_ = elemento[1]
